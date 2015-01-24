71109e6d3d9fabdcfb26704b0f6e91d0b80470ba
Gabriel Reiser <greiser@cardfree.com>
2015-01-23 23:16:12 -0700

    removed docs
    Signed-off-by: Gabriel Reiser <greiser@cardfree.com>

---
+++ docs/rundoc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mdoc update -i ../Reactor/bin/x86/Debug/Reactor.xml -o ./xml ../Reactor/bin/x86/Debug/Reactor.dll
--- rundoc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mdoc update -i Reactor/bin/x64/Debug/Reactor.xml -o docs/en Reactor/bin/x64/Debug/Reactor.dll
-mdoc export-html -o docs/html/en docs/en
